4c0dacc1896b84089564dae770e809580bdea0fc
geoffroystanger <geoffroystanger@users.noreply.github.com>
2023-01-04 20:13:03 +0100

    style: Add style to the scrolling on the projects page

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -4,26 +4,32 @@ import Header from "../components/Header";
 import Project from "../components/Project";
 
 function Projects() {
-    /*const [offset, setOffset] = useState(0);
+    /*
+    Will be used to handle scroll
+    const [offset, setOffset] = useState(0);
 
     useEffect(() => {
         const el = document.getElementById('projects');
         const onScroll = (event) => console.log(event);
         el.addEventListener('scroll', onScroll, {passive: true});
         console.log(offset);
-    }, []);*/
+    }, []);
+    */
     return (
         <Header page="projects">
             <div className="projects-align">
                 <div className="projects">
                     <ul>
-                        <Project link="https://github.com/GeoffroyS6868/adventofcode" title="Advent of Code" date="2022" id="1"></Project>
-                        <Project link="https://github.com/GeoffroyS6868/portfolio" title="Portfolio" date="2023" id="2"></Project>
+                        <div className="empty"></div>
+                        <Project link="https://github.com/GeoffroyS6868/portfolio" title="Portfolio" date="2023" id="1"></Project>
+                        <Project link="https://github.com/GeoffroyS6868/aptos-ranking-bot" title="Aptos Ranking Bot" date="2023" id="2"></Project>
+                        <Project link="https://github.com/GeoffroyS6868/adventofcode" title="AdventOfCode 2022" date="2022" id="3"></Project>
+                        <div className="empty"></div>
                     </ul>
                 </div>
             </div>
         </Header>
-    )
+    );
 }
 
 export default Projects;
