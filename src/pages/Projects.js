43bd301208278b6c227f58db7dff9389bb9a6f62
geoffroystanger <geoffroystanger@users.noreply.github.com>
2023-08-10 16:42:06 +0200

    feat: Update font imports

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -10,8 +10,7 @@ function Projects() {
                 <div className="projects">
                     <ul>
                         <div className="empty"></div>
-                        <Project link="https://github.com/GeoffroyS6868/portfolio" title="Portfolio" date="2023" id="1"></Project>
-                        <Project link="https://github.com/GeoffroyS6868/adventofcode" title="AdventOfCode 2022" date="2022" id="2"></Project>
+                            <Project link="https://github.com/GeoffroyS6868/portfolio" title="Portfolio" date="2023" id="1"></Project>
                         <div className="empty"></div>
                     </ul>
                 </div>
